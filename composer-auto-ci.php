47c2b449fa6d89d979a0e5a5eedeae2ad8cbfedb
Vadim <project.k.vadim@gmail.com>
2023-04-29 17:48:37 +0300

    test git hook "push" script (error)

--- composer-auto-ci.php
+++ composer-auto-ci.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 $output = null;
 $retval = null;
 exec('composer run-script auto-ci', $output, $retval);
-if ($retval < 0) {
+if ($retval != 0) {
     print(implode("\n", $output));
     exit(1);
 }
